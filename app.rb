9457d125f118f758e4b8bf5f92ebf9798d8c7e48
Patryk Pilecki <ppilecki@icloud.com>
2018-08-22 10:02:27 +0100

    basic views, sinatra setup

--- app.rb
+++ app.rb
@@ -1,5 +1,31 @@
+require 'sinatra/base'
 require './database_connection_setup'
 require './lib/offer'
 require './lib/messenger'
 
 class Merchant  < Sinatra::Base
+  run! if app_file == $0
+
+  get '/' do
+    redirect('/offers')
+  end
+
+  get '/offers' do
+    @offers = Offer.show
+    erb :"offers/index"
+  end
+
+  get '/offers/new' do
+    erb :"offers/new"
+  end
+
+  post '/offers' do
+    Offer.create(params)
+    redirect '/offers'
+  end
+
+  post '/offers/:id/cancel' do
+    Offer.cancel(params['id'])
+    redirect '/offers'
+  end
+end
